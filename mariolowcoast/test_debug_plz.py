de65eadcd08e0bca5387e9cb362d9de23ff3348c
samsamsang <95413286+samsamsang@users.noreply.github.com>
2022-12-27 21:11:03 +0100

    Update test_debug_plz.py
    normalement tout est expliqué et ca marche

--- mariolowcoast/test_debug_plz.py
+++ mariolowcoast/test_debug_plz.py
@@ -1,38 +1,40 @@
 
 import  pygame, math, random, os 
-from pygame.image import load
+from pygame.image import load               ## import des modules de base nécessare au jeu
 
-pygame.init()
+pygame.init()           ##lancement de pygame
 
 
-sw=800
+sw=800                  ##définition des dimensions de base de l'écran
 sh=600
 
-run=True
+run=True                ## création des booléens nécessaires à ce jeu
 test_mario=True
 Playscreen=False
-ground= True
-
-bg=pygame.image.load('blabla.png')
+ground= False
+jump=2
+sumersolt=2
+count=1
+bg=pygame.image.load('blabla.png')  ##import des images et fichiers nécessaires.
 bg=pygame.transform.scale(bg, (int(bg.get_width()/4),int(bg.get_height()/4)))
 arrow=pygame.image.load('arrow.png')
 arrow=pygame.transform.scale(arrow, (int(arrow.get_width()/20),int(arrow.get_height()/20)))
 arrow=pygame.transform.rotate(arrow, 90)
 wheel=pygame.image.load('Settings.png')
-bw=bg.get_width()
+bw=bg.get_width()                   ## obtention de la dimension de l'image de base.
 bh=bg.get_height()
-clock=pygame.time.Clock()
-a=0
+clock=pygame.time.Clock()           ## céation de la fonction de temps.
+a=0                                 ## position de l'image de fond par rapport à l'écran.
 b=-bh
-pygame.display.set_caption('mariolowcoast')
+pygame.display.set_caption('mariolowcoast')## nom de la fenêtre pygame ouverte
 screen=pygame.display.set_mode((sw,sh))
 def Screen(a,b):
-    screen.blit(bg,(a,b))
+    screen.blit(bg,(a,b))       ## création de la fenêtre et mise a jour
     pygame.display.update()
 a=0
 b=600-bh
-class maria():
-    def __init__(self):
+class maria():                  ## création du personnage
+    def __init__(self):         ## paramètres de base du personnage (position, image, vitesse)
         self.x=50
         self.y=50
         self.vx=0
@@ -40,7 +42,7 @@ class maria():
         self.mario_img = pygame.image.load('maryo.png')
         self.mario_img_rect = self.mario_img.get_rect()
         self.mario_img_rect.center=(self.x,self.y)
-    def _velocityxr(self):
+    def _velocityxr(self):      ##actions impliquant le petrsonnage
         if self.vx<=10:
             self.vx+=1
     def _velocityxl(self):
@@ -50,21 +52,24 @@ class maria():
         if self.vx!=0:
             self.vx=0
     def _jump(self):
-        self.vy=10
-        while not ground:
-            self.vy-=0.2
         self.vy=0
+        self.vy-=10
+        
+        
+        
     def _fall(self):
         self.vy+=0.2
-    def _draw(self,screen):
+
+    def _draw(self,screen):                             ##apparition de l'image à l'écran
         screen.blit(self.mario_img,self.mario_img_rect)
 
 
 
 
-    def _moveForward(self):
-        self.x += self.vx*0.25
-        self.y +=self.vy*0.25
+    def _moveForward(self):         ##actualisation automatique de la postion du personnage en fonction de la vitesse
+        self.x += self.vx*0.5
+        self.y +=self.vy*0.5
+        self.mario_img_rect.center=(self.x,self.y)
 
 def Begin():
         screen.blit(bg,(a,b))
@@ -95,38 +100,53 @@ def Home():
 mario=maria()
 while  run:
     
-    for event in pygame.event.get():     
+    for event in pygame.event.get():   ## là étape TRES IMPORTANTE, il faut absolument définir cette action pour que la fenêtre se ferme.  
         if event.type==pygame.QUIT:
             run=False
-    if Playscreen:
+    if Playscreen:                      ## définition de l'écran d'accueil
            Begin()
            Title()
            gotooption()
            
-           pygame.display.flip()
-    elif test_mario:
+           pygame.display.flip()        ## raffraichissement de l'image, cela mets à jours tout ce qui apparait à l'écran.
+    elif test_mario:        ##lancement du jeu
         screen.blit(bg,(a,b))
-        
-        mario._moveForward()
-        mario._draw(screen)
+        clock.tick(120)     ##definition de la fréquence de rafraichissement
+        mario._moveForward()##mise à jour de la position du personnage principal
+        mario._draw(screen)## on dessine mario dans la fenêtre
         pygame.display.flip()
-        keys=pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] and a>= 800-bw:
+        keys=pygame.key.get_pressed() ## c'est une fonction qui verifie si une clé est pressée, on puet, en appelant précisement un clé, la faire renvoyer un booléen
+        if keys[pygame.K_RIGHT] and a>= 800-bw:##mouvmeent du personnage de gauche à droite.
             
-            mario.x+=50
+            mario._velocityxr()
             pygame.display.update()
         elif keys[pygame.K_LEFT] and a<= 5:
             
-            mario.y+=50
+            mario._velocityxl()
+    
     
         else:
             mario._brakex()
-        if  ground==False:
+        if keys[pygame.K_SPACE] : ## mise en place du saut
+            if ground==True and jump>0:
+                mario.y-=10
+                mario._jump()
+                jump-=1
+            elif ground==False and sumersolt>0 and count>30:
+                mario._jump()
+                sumersolt-=1
+            
+        if  ground==False:      ##definition du sol et des fonctions nécessaires a la chute et au saut
             mario._fall()
-        if mario.y<1500:
+            count+=1
+        if mario.y<470:
             ground= False
         else:
             ground= True
+            mario.vy=0
+            jump=1
+            sumersolt=1
+            count=1
 
 
             
